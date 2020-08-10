0ddb768e235947c03a3788db4e7e963a2640167d
Andres Bonilla <abonillaa@ucenfotec.ac.cr>
2020-08-10 09:45:20 -0600

    Changes to handle dates on graphql

--- src/main/java/com/cenfotec/movies/mutation/MovieMutation.java
+++ src/main/java/com/cenfotec/movies/mutation/MovieMutation.java
@@ -10,6 +10,8 @@ import com.cenfotec.movies.models.Movie;
 import com.cenfotec.movies.services.MovieService;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;  
 import javassist.NotFoundException;
 
 @Component
@@ -17,19 +19,22 @@ public class MovieMutation  implements GraphQLMutationResolver{
 	@Autowired
 	private MovieService service;
 	
-	public Movie createMovie(String titulo, int presupuesto, float duracion, String lenguaje) {
-		return service.saveMovie(new Movie(null, titulo, presupuesto, duracion, lenguaje, null));
+	public Movie createMovie(String titulo, int presupuesto, float duracion, String lenguaje, String fechaLanzamiento) throws ParseException {
+		Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fechaLanzamiento);
+		return service.saveMovie(new Movie(null, titulo, presupuesto, duracion, lenguaje, fecha));
 	}
 	
-	public Movie updateMovie(long ID, String titulo, int presupuesto, float duracion, String lenguaje) throws NotFoundException {
+	public Movie updateMovie(long ID, String titulo, int presupuesto, float duracion, String lenguaje, String fechaLanzamiento) throws NotFoundException, ParseException {
 		Optional<Movie> optMovie = service.findMovie(ID);
 		if(optMovie.isPresent()) {
 			Movie movie = optMovie.get();
 			
+			Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fechaLanzamiento);
 			movie.setTitulo(titulo);
 			movie.setPresupuesto(presupuesto);
 			movie.setDuracion(duracion);
 			movie.setLenguaje(lenguaje);
+			movie.setFechaLanzamiento(fecha);
 			
 			service.saveMovie(movie);
 			return movie;
